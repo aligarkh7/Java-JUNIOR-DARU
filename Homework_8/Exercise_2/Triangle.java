8f27638b86c5007d53af0ab4984a577e93e5b59e
Ali <reactiveplane@gmail.com>
2024-11-27 02:26:08 +0500

    Change Homework_8/Exercise_2/Triangle.java

--- Homework_8/Exercise_2/Triangle.java
+++ Homework_8/Exercise_2/Triangle.java
@@ -16,7 +16,7 @@ public class Triangle {
     }
 
     public double area() {
-        int p = (a + b + c) / 2;
+        float p = (float) perimeter() / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 }
